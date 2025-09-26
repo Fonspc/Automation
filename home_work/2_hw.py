90bbcd882697697ed7a46fa3a25b5476532546fd
Maxim <maxvasilyev97@mail.ru>
2025-09-26 14:53:24 +0300

    First commit

---
+++ home_work/2_hw.py
@@ -0,0 +1,56 @@
+# Задача 1
+def task_1() -> None:
+    a: int = 34
+    b: float = 1.7
+    c: str = "Мега"
+    d: list = [3, 4, 5]
+    e: bool = True
+
+    print(type(a))
+    print(type(b))
+    print(type(c))
+    print(type(d))
+    print(type(e))
+
+print(task_1())
+
+
+# # Задача 2
+def task_2() -> None:
+    a = [1, 2, 3, 5, 8, 13, 21]
+
+    print(a[0:3])
+
+print(task_2())
+
+
+
+# Задача 3
+def task_3(a: int=5) -> int:
+    return (a** 2)
+
+print(task_3())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
